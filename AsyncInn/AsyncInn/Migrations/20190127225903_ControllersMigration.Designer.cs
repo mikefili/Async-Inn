30cd99b96d96446ca943f266969294b0df6e4b40
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-27 15:07:08 -0800

    db successfully updated

--- AsyncInn/AsyncInn/Migrations/20190127225903_ControllersMigration.Designer.cs
+++ AsyncInn/AsyncInn/Migrations/20190127225903_ControllersMigration.Designer.cs
@@ -4,14 +4,16 @@ using AsyncInn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsyncInn.Migrations
 {
     [DbContext(typeof(AsyncInnDbContext))]
-    partial class AsyncInnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190127225903_ControllersMigration")]
+    partial class ControllersMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
