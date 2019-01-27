0729e048fc20e34f54b3811f75ca7f7eaeed84b3
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-27 14:17:59 -0800

    setup models

---
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class HotelRoom
+    {
+        public int HotelID { get; set; }
+        public int RoomNumber { get; set; }
+        public decimal RoomID { get; set; }
+        public decimal Rate { get; set; }
+        public bool MyProperty { get; set; }
+
+        // navigation properties
+        public ICollection<Hotel> Hotels { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
---
+++ AsyncInn/AsyncInn/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Layout { get; set; }
+
+        // navigation properties
+        public ICollection<HotelRoom> HotelRooms { get; set; }
+        public ICollection<RoomAmenities> RoomAmenities { get; set; }
+    }
+
+    public enum Layouts
+    {
+        Studio,
+        OneBedroom,
+        TwoBedroom
+    }
+}
