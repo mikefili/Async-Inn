2ca4041eeba0d362f35d824b07f5198fbaaf951e
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-28 14:35:48 -0800

    Seeded Data

--- AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/AsyncInn/Data/AsyncInnDbContext.cs
@@ -19,6 +19,110 @@ namespace AsyncInn.Data
         {
             modelBuilder.Entity<HotelRoom>().HasKey(hr => new { hr.HotelID, hr.RoomID });
             modelBuilder.Entity<RoomAmenities>().HasKey(ra => new { ra.AmenitiesID, ra.RoomID });
+
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   ID = 1,
+                   Name = "Downtown Seattle",
+                   Address = "123 1st Ave S, Seattle, WA 98101",
+                   Phone = "12065551234"
+               },
+               new Hotel
+               {
+                   ID = 2,
+                   Name = "Ballard",
+                   Address = "1 Main St, Seattle, WA 98107",
+                   Phone = "12065551234"
+               },
+               new Hotel
+               {
+                   ID = 3,
+                   Name = "Downtown Renton",
+                   Address = "1234 Rainier Ave S, Renton, WA 98057",
+                   Phone = "12065551234"
+               },
+               new Hotel
+               {
+                   ID = 4,
+                   Name = "Bellevue Square",
+                   Address = "1 Bellevue Square, Bellevue, WA 98004",
+                   Phone = "12065551234"
+               },
+               new Hotel
+               {
+                   ID = 5,
+                   Name = "Everett",
+                   Address = "3 Everett Way, Everett, WA 98206",
+                   Phone = "12065551234"
+               }
+               );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    ID = 1,
+                    Name = "Standard Studio",
+                    Layout = Layout.Studio
+                }, new Room
+                {
+                    ID = 2,
+                    Name = "Standard One Bedroom",
+                    Layout = Layout.OneBedroom
+                },
+                new Room
+                {
+                    ID = 3,
+                    Name = "Standard Two Bedroom",
+                    Layout = Layout.TwoBedroom
+                },
+                new Room
+                {
+                    ID = 4,
+                    Name = "Pet Friendly Studio",
+                    Layout = Layout.Studio
+                },
+                new Room
+                {
+                    ID = 5,
+                    Name = "Pet Friendly One Bedroom",
+                    Layout = Layout.OneBedroom
+                },
+                new Room
+                {
+                    ID = 6,
+                    Name = "Pet Friendly Two Bedroom",
+                    Layout = Layout.TwoBedroom
+                }
+                );
+
+            modelBuilder.Entity<Amenities>().HasData(
+                new Amenities
+                {
+                    ID = 1,
+                    Name = "Air Conditioning"
+                },
+                new Amenities
+                {
+                    ID = 2,
+                    Name = "Kitchenette"
+                },
+                new Amenities
+                {
+                    ID = 3,
+                    Name = "Jacuzzi Tub"
+                },
+                new Amenities
+                {
+                    ID = 4,
+                    Name = "Balcony"
+                },
+                new Amenities
+                {
+                    ID = 5,
+                    Name = "Office"
+                }
+                );
         }
 
         public DbSet<Hotel> Hotels { get; set; }
---
+++ AsyncInn/AsyncInn/Migrations/20190128223246_SeededData.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class SeededData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Air Conditioning" },
+                    { 2, "Kitchenette" },
+                    { 3, "Jacuzzi Tub" },
+                    { 4, "Balcony" },
+                    { 5, "Office" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "ID", "Address", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "123 1st Ave S, Seattle, WA 98101", "Downtown Seattle", "12065551234" },
+                    { 2, "1 Main St, Seattle, WA 98107", "Ballard", "12065551234" },
+                    { 3, "1234 Rainier Ave S, Renton, WA 98057", "Downtown Renton", "12065551234" },
+                    { 4, "1 Bellevue Square, Bellevue, WA 98004", "Bellevue Square", "12065551234" },
+                    { 5, "3 Everett Way, Everett, WA 98206", "Everett", "12065551234" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Standard Studio" },
+                    { 2, 1, "Standard One Bedroom" },
+                    { 3, 2, "Standard Two Bedroom" },
+                    { 4, 0, "Pet Friendly Studio" },
+                    { 5, 1, "Pet Friendly One Bedroom" },
+                    { 6, 2, "Pet Friendly Two Bedroom" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 6);
+        }
+    }
+}
