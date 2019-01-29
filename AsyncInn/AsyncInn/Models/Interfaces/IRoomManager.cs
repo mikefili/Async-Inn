a7532e815a384765400639ff7be08d5d54234914
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-29 12:54:27 -0800

    IRoomManager CRUD ops scaffolded out

--- AsyncInn/AsyncInn/Models/Interfaces/IRoomManager.cs
+++ AsyncInn/AsyncInn/Models/Interfaces/IRoomManager.cs
@@ -8,15 +8,17 @@ namespace AsyncInn.Models.Interfaces
     interface IRoomManager
     {
         // Create a room
-
+        Task CreateRoom(Room room);
 
         // Read a room
+        Task<Room> GetRoom(int id);
 
+        Task<IEnumerable<Room>> GetRooms();
 
         // Update a room
-
+        void UpdateRoom(Room room);
 
         // Delete a room
-
+        void DeleteRoom(int id);
     }
 }
