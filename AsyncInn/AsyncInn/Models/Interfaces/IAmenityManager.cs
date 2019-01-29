b6302f13ccd075862b1c1714abf789e2febd3842
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-29 12:56:52 -0800

    IAmenityManager CRUD ops scaffolded out

--- AsyncInn/AsyncInn/Models/Interfaces/IAmenityManager.cs
+++ AsyncInn/AsyncInn/Models/Interfaces/IAmenityManager.cs
@@ -8,15 +8,17 @@ namespace AsyncInn.Models.Interfaces
     interface IAmenityManager
     {
         // Create an amenity
-
+        Task CreateAmenity(Amenities amenities);
 
         // Read an amenity
+        Task<Amenities> GetAmenity(int id);
 
+        Task<IEnumerable<Amenities>> GetAmenities();
 
         // Update an amenity
+        void UpdateAmenity(Amenities amenities);
 
-        
         // Delete an amenity
-
+        void DeleteAmenity(int id);
     }
 }
