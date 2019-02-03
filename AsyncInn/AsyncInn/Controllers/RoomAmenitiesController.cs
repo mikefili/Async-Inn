dc1fbe09a0ae1cf0bc55b84313e5a361436580f6
Michael Filicetti <filicetti.michael@gmail.com>
2019-02-03 15:01:00 -0800

    room count not working but should be. frustrated.

--- AsyncInn/AsyncInn/Controllers/RoomAmenitiesController.cs
+++ AsyncInn/AsyncInn/Controllers/RoomAmenitiesController.cs
@@ -55,8 +55,6 @@ namespace AsyncInn.Controllers
         }
 
         // POST: RoomAmenities/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("AmenitiesID,RoomID")] RoomAmenities roomAmenities)
